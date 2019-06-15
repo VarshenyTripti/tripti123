35907d5a782bcede5aa3a173d229f493a860504f
VarshenyTripti <51847343+VarshenyTripti@users.noreply.github.com>
2019-06-15 18:48:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+ <head>
+  <title>Sudoku</title>
+  <link href="css/sudoku-style.css" rel="stylesheet" type="text/css">
+  <link href="css/animate.css" rel="stylesheet" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+  <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-114x114-precomposed.png"/>
+  <link href="images/favicon.ico" rel="shortcut icon">
+
+ </head>
+
+ <body>
+  
+  <div id="header" class="header">sudoku</div>
+
+  <div id="welcome" class="welcome">
+    <h1>sudoku</h1>
+    <h2>by <a href="/" target="new">adam</a></h2>
+    <button onclick="Sudoku.game.init()">new game</button>
+  </div>
+
+  <div id="game-message" class="game-message"></div>
+
+  <div id="number-picker" class="number-picker"></div>
+  
+  <div id="solving-status" class="solving-status"></div>
+
+  <div id="board" class="board"></div>
+
+  <div id="buttons" class="buttons">
+   <button id="start-button" class="start" onclick="Sudoku.game.init()">start new game</button>
+   <button id="solve-button" class="solve" onclick="Sudoku.game.validateBeforeSolve()">solve board</button>
+  </div>
+
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/underscore.js"></script>
+  <script type="text/javascript" src="js/sudoku.js"></script>
+
+ </body>
+
+</html>
